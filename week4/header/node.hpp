d67f424f25b8c604b641f48ce8b824f6ef6d78ed
Hengg <songhengkim93@gmail.com>
2025-10-21 10:29:41 +0700

    Changes to be committed: 	modified:   week4/header/node.hpp

--- week4/header/node.hpp
+++ week4/header/node.hpp
@@ -1,4 +1,15 @@
 #ifndef node
 #define node
+class onec_node{
+    public:
+        int val;
+        onec_node* next;
 
+};
+class doub_node{
+    public:
+        int val;
+        doub_node* next;
+        doub_node* prev;
+}
 #endif 
